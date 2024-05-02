3602446c1a7ed7a610cd277b592c0b568bc8bf2c
Rex You <rexyou0831@gmail.com>
2024-05-02 16:12:45 +0800

    Adjust cart view display

--- resources/js/views/cartView.vue
+++ resources/js/views/cartView.vue
@@ -6,13 +6,31 @@
         <div class="cart_container">
             <div v-if="cartList.length > 0" class="cart_list">
                 <div v-for="(cart, index) in cartList" :key="index" class="cart_item">
+                    <div class="product_image" v-if="cart.product.icon_image_url != null" :style="{ background: `url('${cart.product.icon_image_url}')` }"></div>
+                    <div class="product_image" v-if="cart.product.product_images_filter != null" :style="{ background: `url('${cart.product.product_images_filter[0].image_url}')` }"></div>
                     <div class="product_detail">
                         <h1>{{ cart.product.name }}</h1>
-                        <h1>{{ cart.product_id }}</h1>
-                        <p>{{ cart.product_option_details.options }}</p>
+                        <div v-if="cart.product_option_details.options != null" class="selected_option_box">
+                            <div v-for="(value, key) in cart.product_option_details.options" :key="key" class="selected_options">
+                                <span>{{ key }} : {{ value }}</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="product_quantity"></div>
+                    <div class="product_quantity">
+                        <button @click.prevent="adjustItemQuantity('minus')">
+                            <v-icon name="hi-minus-circle"/>
+                        </button>
+                        <div class="current_quantity">
+                            <span>{{ cart.quantity }}</span>
+                        </div>
+                        <button @click.prevent="adjustItemQuantity('add')">
+                            <v-icon name="hi-plus-circle"/>
+                        </button>
+                    </div>
                 </div>
+            </div>
+            <div v-else class="cart_list2">
+                <h1>Currently dont have item inside cart</h1>
             </div>
             <div class="calculation">
                 <div class="calculation_board">
@@ -63,6 +81,9 @@
         estimate_price: 0
     })
 
+    const adjustItemQuantity = (action) => {
+        console.log("current_action: ", action)
+    }
 </script>
 
 <style scoped>
@@ -100,29 +121,91 @@
         z-index: 1;
     }
 
-    .cart_list {
+    .cart_list,
+    .cart_list2 {
         width: 70%;
         height: auto;
         padding: 10px 20px;
     }
 
+    .cart_list2 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
     .cart_item {
         width: 100%;
-        border: 1px solid red;
+        height: 180px;
+        margin: 30px auto;
+        display: flex;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+
+    .product_image {
+        /* background: url("https://images.unsplash.com/photo-1545127398-14699f92334b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"); */
+        background-size: cover !important;
+        background-position: center !important;
+        width: 30%;
         height: auto;
-        margin: 20px auto;
+    }
+
+    .product_detail {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 50px;
+    }
+
+    .product_detail h1 {
+        font-size: 24px;
+        text-overflow: ellipsis;
+    }
+
+    .selected_option_box {
+        margin: 10px 0px;
+    }
+
+    .selected_options {
+        font-size: 16px;
+        margin: 5px 0px;
+        color: #888888;
+    }
+
+    .product_quantity {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        border-left: 1px solid rgba(0, 0, 0, 0.2);
+        width: 20%;
+    }
+
+    .product_quantity button {
+        border: none;
+        box-shadow: none;
+        background: none;
+    }
+
+    .product_quantity button svg {
+        width: 40px;
+        height: 40px;
+    }
+
+    .current_quantity {
+        font-size: 24px;
     }
 
     .calculation {
         width: 30%;
-        padding: 30px 20px;
+        padding: 40px 20px;
         position: relative;
     }
 
     .calculation_board {
         position: -webkit-sticky; /* Safari */
         position: sticky;
-        top: 30px;
+        top: 40px;
         border: 1px solid rgba(0, 0, 0, 0.4);
         padding: 30px 20px;
     }
